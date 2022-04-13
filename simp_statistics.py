4ea39e292f950df6dd2217612fdefdc2aa439902
Cherrypick14 <cherrypicklet@gmail.com>
2022-04-13 23:32:26 +0300

    simple_stats

---
+++ simp_statistics.py
@@ -0,0 +1,11 @@
+#   simple stats on a list of values
+
+import numpy as np
+
+listing = [1,2,3,4,5]
+
+print("average: ", np.average(listing))
+print("variance: ", np.var(listing))
+print("mean: ", np.mean(listing))
+print("median: ", np.median(listing))
+

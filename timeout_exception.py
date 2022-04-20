c7a6789a2772f7ef47f35caa34bceab882c4e1bd
Cherrypick14 <cherrypicklet@gmail.com>
2022-04-20 23:56:54 +0300

    changed variable

--- timeout_exception.py
+++ timeout_exception.py
@@ -4,13 +4,13 @@ import requests
 def timeout_request():
 
     try:
-        c = requests.get('https://github.com/', timeout= 0.0001)
+        r = requests.get('https://github.com/', timeout= 0.0001)
     #   print(c.text)
     except requests.exceptions as error:
         print(error)
 
     try:
-        c = requests.get('https://github.com/', timeout=1.0)
+        r = requests.get('https://github.com/', timeout=1.0)
         print("you're connected!,,,,")
     except requests.exceptions as error:
         print(error)
